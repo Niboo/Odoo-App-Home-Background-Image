32e8e5311ec83d02399a69434da78b9f9d582195
Janik von Rotz <login@janikvonrotz.ch>
2021-10-05 12:26:57 +0200

    [IMP] lint module

--- __manifest__.py
+++ __manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Home background image",
 
@@ -6,15 +5,11 @@
         Set a background image for the Odoo company.
     """,
 
-    'description': """
-        Set a background image for the Odoo company.
-        The background image is shown in the Odoo backend only.
-    """,
-
-    'author': "Mint System GmbH",
+    'author': "Mint System GmbH, Odoo Community Association (OCA)",
     'website': "https://www.mint-system.ch",
     'category': 'Customizations',
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.1.0',
+    'license': 'AGPL-3',
 
     'depends': ['base'],
 
